e22e6d81ae4206e4e924b2eb29740f6d14d53c3d
jasm1nl <jasminliangstudio@gmail.com>
2022-04-17 14:50:23 -0400

    VR rig

---
+++ Assets/Code/VRInit.cs
@@ -0,0 +1,36 @@
+//Run this on the first scene loaded. You only need to run this once.
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.XR;
+
+public class VRInit : MonoBehaviour
+{
+    private List<InputDevice> inputDevices = new List<InputDevice>();
+    private List<XRInputSubsystem> subsystems = new List<XRInputSubsystem>();
+
+    private void Awake()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        InputDevices.GetDevices(inputDevices);
+        if (inputDevices[0].manufacturer == "Oculus")
+        {
+            Unity.XR.Oculus.Performance.TrySetDisplayRefreshRate(90);
+            //Unity.XR.Oculus.Performance.TrySetDisplayRefreshRate(120);
+        }
+#endif
+    }
+
+    IEnumerator Start()
+    {
+        yield return null;
+        SubsystemManager.GetInstances(subsystems);
+        foreach (var subsystem in subsystems)
+        {
+            subsystem.TrySetTrackingOriginMode(TrackingOriginModeFlags.Floor);
+        }
+    }
+}
