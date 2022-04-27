dfd9c76398ffb16417f4dc6b2d3ba54025b595e2
11384antont <11384antont@gmail.com>
2022-04-27 17:14:45 -0400

    Pot code changed (no destroy), added tags and colliders to ingredients

--- Assets/Code/Pot.cs
+++ Assets/Code/Pot.cs
@@ -5,8 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Pot : MonoBehaviour
 {
-    string currentRecipeString = "";
-    string recipe = "mushroom" + "eyeball" + "boot";
+    // string currentRecipeString = "";
+    // string recipe = "mushroom" + "eyeball" + "boot";
 
     // ^you can change the recipe here
     bool cooked = false;
@@ -15,7 +15,13 @@ public class Pot : MonoBehaviour
     public GameObject liquid;
     AudioSource addingSound;
 
-    int itemsAdded = 0;
+    bool item1 = false;
+    bool item2 = false;
+    bool item3 = false;
+    bool item4 = false;
+    bool item5 = false;
+    bool item6 = false;
+    bool item7 = false;
 
     // public GameObject text;
 
@@ -27,31 +33,62 @@ public class Pot : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Grabbable"))
+        if (other.gameObject.CompareTag("mushroom") && !item1)
         {
-            currentRecipeString += other.gameObject.tag;
+            item1 = true;
             liquid.SetActive(true);
             addingSound.Play();
-            itemsAdded++;
-            Destroy(other.gameObject);
-
-            if (currentRecipeString == recipe && !cooked)
-            {
-                potion.SetActive(true);
-                gameObject.SetActive(false);
-                StartCoroutine(WaitTime());
-                cooked = true;
-            }
+            // Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("eyeball") && !item2 && item1 == true)
+        {
+            item2 = true;
+            addingSound.Play();
+        }
+        if (other.gameObject.CompareTag("boot") && !item3 && item2 == true)
+        {
+            item3 = true;
+            addingSound.Play();
+        }
+        if (other.gameObject.CompareTag("watermelon") && !item4 && item3 == true)
+        {
+            item4 = true;
+            addingSound.Play();
+        }
+        if (other.gameObject.CompareTag("shampoo") && !item5 && item4 == true)
+        {
+            item5 = true;
+            addingSound.Play();
+        }
+        if (other.gameObject.CompareTag("hammer") && !item6 && item5 == true)
+        {
+            item6 = true;
+            addingSound.Play();
+        }
+        if (other.gameObject.CompareTag("dskull") && !item7 && item6 == true)
+        {
+            item7 = true;
+            addingSound.Play();
+        }
+        if (other.gameObject.CompareTag("berries") && item7 == true)
+        {
+            cooked = true;
+            addingSound.Play();
+        }
+        if (cooked == true)
+        {
+            potion.SetActive(true);
+            liquid.SetActive(false);
         }
     }
 
     private void Update()
     {
-        if (cooked == true || itemsAdded == 3)
-        {
-            StartCoroutine(WaitTime());
-            Destroy(gameObject);
-        }
+        // if (cooked == true || itemsAdded == 3)
+        // {
+        //     StartCoroutine(WaitTime());
+        //     Destroy(gameObject);
+        // }
     }
 
     IEnumerator WaitTime()
