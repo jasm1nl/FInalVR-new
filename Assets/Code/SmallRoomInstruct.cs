14eb05256feff25bf287ef4ddb8bc42f1233c9e5
jasm1nl <jasminliangstudio@gmail.com>
2022-04-27 20:06:07 -0400

    Added small room to scene 1

---
+++ Assets/Code/SmallRoomInstruct.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallRoomInstruct : MonoBehaviour
+{
+    public GameObject puzzleUI;
+
+    private void OnCollisionEnter(Collision other) {
+        
+        if(other.gameObject.CompareTag("Player")){
+            puzzleUI.SetActive(true); 
+            StartCoroutine(UIlifetime());
+        }
+
+    IEnumerator UIlifetime(){
+        yield return new WaitForSeconds(.5f);
+        puzzleUI.SetActive(false);
+
+        }
+    }
+}
