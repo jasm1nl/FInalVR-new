743b55d7e02ffa54bb115d15af72036bf90dcbf7
jasm1nl <jasminliangstudio@gmail.com>
2022-05-08 00:21:53 -0400

    Added torch code, light puzzle and small room door

--- Assets/Code/GlobalLightCheck.cs
+++ Assets/Code/GlobalLightCheck.cs
@@ -16,11 +16,23 @@ public class GlobalLightCheck : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(light1.activeInHierarchy && !light2.activeInHierarchy && !light3.activeInHierarchy && 
-        light4.activeInHierarchy && light5.activeInHierarchy && !light6.activeInHierarchy && !light7.activeInHierarchy)
+        // if(light1.activeInHierarchy && !light2.activeInHierarchy && !light3.activeInHierarchy && 
+        // light4.activeInHierarchy && light5.activeInHierarchy && !light6.activeInHierarchy && !light7.activeInHierarchy)
+        // {
+        //     //remove the door from small room 
+        //     Destroy(Smalldoor);
+        // }
+
+        if(light1.gameObject.GetComponent<Light>().intensity==8 && 
+            light2.gameObject.GetComponent<Light>().intensity==0 && 
+            light3.gameObject.GetComponent<Light>().intensity==0 && 
+            light4.gameObject.GetComponent<Light>().intensity==8 && 
+            light5.gameObject.GetComponent<Light>().intensity==8 && 
+            light6.gameObject.GetComponent<Light>().intensity==0 && 
+            light7.gameObject.GetComponent<Light>().intensity==0)
         {
             //remove the door from small room 
-            Destroy(Smalldoor);
+            Smalldoor.gameObject.SetActive(false);
         }
     }
 }
--- Assets/Code/LightSwitch.cs
+++ Assets/Code/LightSwitch.cs
@@ -8,9 +8,10 @@ public class LightSwitch : MonoBehaviour
     public XRNode handRole = XRNode.RightHand;
     public LayerMask lightingLayer;
     private float raycastDist = 30;
-    private GameObject GameObjectHit = null;
+    private Light PLight;
 
-    private Transform hoverObject = null;
+    //private GameObject GameObjectHit = null;
+    //private Transform hoverObject = null;
 
     //public GameObject Light;
     bool triggerState = false;
@@ -29,15 +30,26 @@ public class LightSwitch : MonoBehaviour
             {
               //Set light inactive 
               //What should I do here to deactivate the gameObject that the raycast hit on?
+                PLight = hit.transform.gameObject.GetComponent<Light>();
 
-                if(hit.transform.gameObject.activeInHierarchy)
-                    {
-                        hit.transform.gameObject.SetActive(false);
-                    }
-                else
-                    {
-                        hit.transform.gameObject.SetActive(true);
-                    }
+                if(PLight.intensity==8)
+                {
+                    PLight.intensity=0;
+                }
+                else if (PLight.intensity==0)
+                {
+                    PLight.intensity=8;
+                }
+
+
+                // if(hit.transform.gameObject.activeInHierarchy)
+                //     {
+                //         hit.transform.gameObject.SetActive(false);
+                //     }
+                // else
+                //     {
+                //         hit.transform.gameObject.SetActive(true);
+                //     }
                 
             }
             
@@ -46,31 +58,31 @@ public class LightSwitch : MonoBehaviour
     }
 
     //Checking raycast hit
-    private void FixedUpdate()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, raycastDist, lightingLayer))
-        {
-            if (hoverObject != hit.transform)
-            {
-                if (hoverObject != null)
-                {
-                    hoverObject.GetComponent<Renderer>().material.color = Color.white;
-                }
-                hoverObject = hit.transform;
-                hoverObject.GetComponent<Renderer>().material.color = Color.green;
-            }
-        } 
+    // private void FixedUpdate()
+    // {
+    //     RaycastHit hit;
+    //     if (Physics.Raycast(transform.position, transform.forward, out hit, raycastDist, lightingLayer))
+    //     {
+    //         if (hoverObject != hit.transform)
+    //         {
+    //             if (hoverObject != null)
+    //             {
+    //                 hoverObject.GetComponent<Renderer>().material.color = Color.white;
+    //             }
+    //             hoverObject = hit.transform;
+    //             hoverObject.GetComponent<Renderer>().material.color = Color.green;
+    //         }
+    //     } 
        
-        else
-        {
-            if (hoverObject != null)
-            {
-                hoverObject.GetComponent<Renderer>().material.color = Color.white;
-                hoverObject = null;
-            }
-        }
-    }
+    //     else
+    //     {
+    //         if (hoverObject != null)
+    //         {
+    //             hoverObject.GetComponent<Renderer>().material.color = Color.white;
+    //             hoverObject = null;
+    //         }
+    //     }
+    // }
 
     
 }
